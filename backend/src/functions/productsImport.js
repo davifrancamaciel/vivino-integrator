237a6045803fb75dd1cdf75dbb222f6ded752628
Davi Maciel <davi.maciel@nagra.com>
2022-07-29 15:44:15 -0300

    Adição de metodo para update em tudo

--- backend/src/functions/productsImport.js
+++ backend/src/functions/productsImport.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { Op } = require('sequelize');
 const db = require('../database');
 const Product = require('../models/Product')(db.sequelize, db.Sequelize);
 const { handlerResponse, handlerErrResponse } = require("../utils/handleResponse");
@@ -22,62 +23,11 @@ module.exports.import = async (event) => {
         if (!process.env.IS_OFFLINE)
             return handlerResponse(200, {}, 'Ops isso só pode rodar local hahahahha')
 
-        let array = []
-        for (let i = 0; i < seed.length; i++) {
-            const element = seed[i];
+        // const result = await seeds();
+        const result = await updateAll();
 
-            const product = {
-                id: Number(element.ID),
-                productName: element.PRODUTO,
-                price: element.PRECO,
-                quantityIsMinimum: element.QUANTIDADE_POR_CAIXA > 1,
-                bottleSize: null,
-                bottleQuantity: element.QUANTIDADE_POR_CAIXA,
-                link: element.URL_PRODUTO,
-                inventoryCount: element.ESTOQUE_ATUAL,
-                producer: element.PRODUTOR,
-                wineName: element.PRODUTO,
-                appellation: null,
-                vintage: element.SAFRA,
-                country: element.PAIS_CUSTOM,
-                color: element.SAFRA,
-                image: element.IMAGEM,
-                ean: element.EAN_GTIN_UPC,
-                description: element.CARACTERISTICAS,
-                alcohol: element.GRAU_ALCOOLICO,
-                producerAddress: element.REGIAO_PRODUTORA,
-                importerAddress: null,
-                varietal: null,
-                ageing: element.ENVELHECIMENTO,
-                closure: null,
-                winemaker: null,
-                productionSize: null,
-                residualSugar: null,
-                acidity: null,
-                ph: null,
-                containsMilkAllergens: false,
-                containsEggAllergens: false,
-                nonAlcoholic: false,
-                active: element.DISPONIVEL && element.DISPONIVEL.toLowerCase() === 'sim' ? true : false
-            };
-            array.push(product)
 
-            // const item = await Product.findByPk(product.id);
-            // if (item) {
-            //     product.price = item.price;
-            //     await item.update(product);
-            // }
-            // else
-            //     await Product.create(product);
-
-            if (element.DATA_CADASTRO) {
-                const date = formatDateCreatePtToEn(element.DATA_CADASTRO);
-                const query = ` UPDATE products AS p SET p.createdAt = '${date}' WHERE p.id = '${product.id}'`;
-                await executeUpdate(query);
-            }
-        }
-
-        return handlerResponse(201, array, `${RESOURCE_NAME} criado com sucesso`)
+        return handlerResponse(201, result, `${RESOURCE_NAME} criado com sucesso`)
     } catch (err) {
         return handlerErrResponse(err)
     }
@@ -87,4 +37,86 @@ const formatDateCreatePtToEn = (value) => {
     const [date, hour] = value.split(' ')
     const [day, month, year] = date.split('/')
     return `${year}-${month}-${day} ${hour}`
+}
+
+const seeds = async () => {
+    let array = []
+    for (let i = 0; i < seed.length; i++) {
+        const element = seed[i];
+
+        const product = {
+            id: Number(element.ID),
+            productName: element.PRODUTO,
+            price: element.PRECO,
+            quantityIsMinimum: element.QUANTIDADE_POR_CAIXA > 1,
+            bottleSize: null,
+            bottleQuantity: element.QUANTIDADE_POR_CAIXA,
+            link: element.URL_PRODUTO,
+            inventoryCount: element.ESTOQUE_ATUAL,
+            producer: element.PRODUTOR,
+            wineName: element.PRODUTO,
+            appellation: null,
+            vintage: element.SAFRA,
+            country: element.PAIS_CUSTOM,
+            color: element.SAFRA,
+            image: element.IMAGEM,
+            ean: element.EAN_GTIN_UPC,
+            description: element.CARACTERISTICAS,
+            alcohol: element.GRAU_ALCOOLICO,
+            producerAddress: element.REGIAO_PRODUTORA,
+            importerAddress: null,
+            varietal: null,
+            ageing: element.ENVELHECIMENTO,
+            closure: null,
+            winemaker: null,
+            productionSize: null,
+            residualSugar: null,
+            acidity: null,
+            ph: null,
+            containsMilkAllergens: false,
+            containsEggAllergens: false,
+            nonAlcoholic: false,
+            active: element.DISPONIVEL && element.DISPONIVEL.toLowerCase() === 'sim' ? true : false
+        };
+        array.push(product)
+
+        // const item = await Product.findByPk(product.id);
+        // if (item) {
+        //     product.price = item.price;
+        //     await item.update(product);
+        // }
+        // else
+        //     await Product.create(product);
+
+        if (element.DATA_CADASTRO) {
+            const date = formatDateCreatePtToEn(element.DATA_CADASTRO);
+            const query = ` UPDATE products AS p SET p.createdAt = '${date}' WHERE p.id = '${product.id}'`;
+            await executeUpdate(query);
+        }
+        return array;
+    }
+}
+
+const updateAll = async () => {
+    const resp = await Product.findAll({
+        attibutes: ['id', 'alcohol'],
+        order: [['id', 'DESC']],
+        limit: 2000,
+    })
+
+
+    const arr = resp.filter(x => x.alcohol != null && !x.alcohol.includes('%'))
+
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        const item = await Product.findByPk(element.id);
+
+        const product = {
+            ...item,
+            alcohol: `${item.alcohol}%`
+        }
+        await item.update(product);
+
+    }
+    return arr
 }
